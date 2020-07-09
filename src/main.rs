fa40eab0d2d0de7a15ab17328f8bddf3aba05c98
Bogdan Lipuš <bogdan.lipus@um.si>
2020-07-09 15:38:29 +0200

    Refactoring

--- src/main.rs
+++ src/main.rs
@@ -3,137 +3,7 @@ use std::fmt;
 use std::mem;
 use std::str::FromStr;
 
-fn reverse_words(input: &str) -> String {
-    let v: Vec<&str> = input.split(" ").collect();
-    let mut output: String = String::from("");
-
-    for x in v {
-        let word = x.trim();
-        if word.len() > 0 {
-            output += " ";
-            output += &word.chars().rev().collect::<String>();
-        } else {
-            output += " ";
-        }
-    }
-    return String::from(output.trim());
-}
-
-fn reverse_words2(input: &str) -> String {
-    String::from(input)
-        .split(" ")
-        .map(|sub| sub.chars().rev().collect())
-        .collect::<Vec<String>>()
-        .join(" ")
-}
-
-fn duplicate_encode(word: &str) -> String {
-    let mut map: HashMap<char, i8> = HashMap::new();
-    let s = String::from(word.to_lowercase());
-    for ch in s.chars() {
-        let count = map.entry(ch).or_insert(0);
-        *count += 1;
-    }
-
-    println!("{:?}", map);
-
-    s.chars()
-        .map(|ch| match *map.get(&ch).unwrap() {
-            1 => '(',
-            _ => ')',
-        })
-        .collect()
-}
-
-fn stock_list(list_art: Vec<&str>, list_cat: Vec<&str>) -> String {
-    let mut map: HashMap<char, i32> = HashMap::new();
-    for s in list_art {
-        let item: Vec<&str> = s.split(" ").collect();
-        let c: char = item[0].chars().next().unwrap();
-        let num: i32 = std::str::FromStr::from_str(item[1]).unwrap();
-        let count = map.entry(c).or_insert(0);
-        *count += num;
-    }
-
-    if map.len() == 0 {
-        return String::from("");
-    }
-    let mut result = String::from("");
-
-    for (index, cat) in list_cat.iter().enumerate() {
-        let c: char = cat.chars().next().unwrap();
-        let count = match map.get(&c) {
-            Some(count) => *count,
-            None => 0i32,
-        };
-
-        result.push_str(format!("({}:{})", c, count).as_str());
-        if index < list_cat.len() - 1 {
-            result.push_str(" - ");
-        }
-    }
-
-    return result;
-}
-
-fn race(v1: i32, v2: i32, g: i32) -> Option<Vec<i32>> {
-    if v1 >= v2 {
-        return None;
-    } else {
-        let mut seconds = g * 3600 / (v2 - v1);
-        let hours = seconds / 3600;
-        seconds -= hours * 3600;
-        let minutes = seconds / 60;
-        seconds -= minutes * 60;
-
-        Some(vec![hours, minutes, seconds])
-    }
-}
-
-fn gps(s: i32, x: Vec<f64>) -> i32 {
-    /*let speed = (|dx:f64,s| (3600f64 * dx)/s );
-    let mut maxSpeed = 0f64;
-    for i in 1..x.len() {
-        let dx = x[i]-x[i-1];
-        let sp = speed(dx,s as f64);
-        if sp > maxSpeed{
-            maxSpeed = sp;
-        }
-    }
-    return maxSpeed.floor() as i32;*/
-    x.iter()
-        .zip(x.iter().skip(1))
-        .map(|(a, b)| (b - a) * 3600.0 / (s as f64))
-        .fold(0.0, f64::max)
-        .floor() as i32
-}
-
-fn good_vs_evil(good: &str, evil: &str) -> String {
-    let good_worth = vec![1, 2, 3, 3, 4, 10];
-    let evil_worth = vec![1, 2, 2, 2, 3, 5, 10];
-
-    let good_result: i32 = good
-        .split_whitespace()
-        .map(|s| i32::from_str(s).unwrap())
-        .zip(good_worth.iter())
-        .map(|(a, b)| a * b)
-        .sum();
-
-    let evil_result: i32 = evil
-        .split_whitespace()
-        .map(|s| i32::from_str(s).unwrap())
-        .zip(evil_worth.iter())
-        .map(|(a, b)| a * b)
-        .sum();
-
-    if good_result > evil_result {
-        return String::from("Battle Result: Good triumphs over Evil");
-    } else if good_result < evil_result {
-        return String::from("Battle Result: Evil eradicates all trace of Good");
-    }
-
-    return String::from("Battle Result: No victor on this battle field");
-}
+mod codewars;
 
 fn literals_operator() {
     println!("1 + 2 = {}", 1i32 + 2);
@@ -354,6 +224,10 @@ fn test_enum() {
 }
 
 fn main() {
+
+
+    println!("{}", codewars::cw01::reverse_words2("To je test"));
+
     /* println!("{}", duplicate_encode("Success"));
 
     println!("{}", reverse_words2("To je test"));*/
@@ -383,5 +257,5 @@ fn main() {
 
     //arrays_slices();
     // struct_test();
-    test_enum();
+    //test_enum();
 }
